0be38837becf246516edd6cfa1622c29e819e2ba
drgeek <mngmnt.hasannejad@gmail.com>
2022-10-28 03:19:22 +0330

    responsive a bit

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
     import drugs from './assets/list_for_game.json'
     import {SvelteToast, toast} from '@zerodevx/svelte-toast'
 
-    let first_choice, second_choice, choice_count = 0;
+    let first_choice, second_choice, choice_count = 0, points = 0, is_won = false;
 
     function choose(choices) {
         let index = Math.floor(Math.random() * choices.length);
@@ -61,7 +61,7 @@
 
     const getCell = (x, y) => {
         reshaped_cells[x][y].selected = true
-
+        console.log(reshaped_cells[x][y])
         if (choice_count === 0) {
             first_choice = reshaped_cells[x][y]
             choice_count = 1
@@ -74,6 +74,10 @@
                 let second_choice_pos = findWithValue(second_choice)
                 reshaped_cells[first_choice_pos['x']][first_choice_pos['y']].done = true
                 reshaped_cells[second_choice_pos['x']][second_choice_pos['y']].done = true
+                points += 1
+                if (points === chosen_drugs.length) {
+                    is_won = true
+                }
             } else {
                 toast.push('False')
             }
@@ -105,34 +109,47 @@
 
 </script>
 <SvelteToast/>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            points : {points}
+        </div>
+    </div>
+</div>
 <div class="container-fluid">
-    {#each reshaped_cells as row,x}
-        <div class="row">
-            {#each row as drug ,y}
-                {#if drug.done}
-                    <div class="col-lg-2 col-md-4 col-sm-6 p-1 justify-content-center text-center py-4">
-                        <p class="my-4">done</p>
-                    </div>
-                {:else}
-                    {#if drug.type === 'text'}
-                        <div class="col-lg-2 col-md-4 col-sm-6 p-1">
-                            <button bind:this={card_holder[x][y]}
-                                    class="h-100 w-100 structure-image {reshaped_cells[x][y].selected?'structure-image-selected' : ''}"
-                                    on:click={()=>{getCell(x,y)}}>
-                                {drug.value}
-                            </button>
+    {#if is_won}
+        <div class="align-content-center h-25">
+            <h3>Congrats , You won</h3>
+        </div>
+    {:else}
+        {#each reshaped_cells as row,x}
+            <div class="row">
+                {#each row as drug ,y}
+                    {#if drug.done}
+                        <div class="col-lg-2 col-md-4 col-sm-6 py-3 done-cell h-100 ">
+                            <p class="my-4">done</p>
                         </div>
                     {:else}
-                        <div class="col-lg-2 col-md-4 col-sm-6 p-1">
-                            <img src={'/drugs/'+drug.value+'.png'} alt=""
-                                 class="structure-image  {reshaped_cells[x][y].selected?'structure-image-selected' : ''}"
-                                 on:click={()=>{getCell(x,y)}}>
-                        </div>
+                        {#if drug.type === 'text'}
+                            <div class="col-lg-2 col-md-4 col-sm-6 p-1">
+                                <button bind:this={card_holder[x][y]}
+                                        class="h-100 w-100 structure-image {reshaped_cells[x][y].selected?'structure-image-selected' : ''}"
+                                        on:click={()=>{getCell(x,y)}}>
+                                    {drug.value}
+                                </button>
+                            </div>
+                        {:else}
+                            <div class="col-lg-2 col-md-4 col-sm-6 p-1">
+                                <img src={'/drugs/'+drug.value+'.png'} alt=""
+                                     class="structure-image  {reshaped_cells[x][y].selected?'structure-image-selected' : ''}"
+                                     on:click={()=>{getCell(x,y)}}>
+                            </div>
+                        {/if}
                     {/if}
-                {/if}
-            {/each}
-        </div>
-    {/each}
+                {/each}
+            </div>
+        {/each}
+    {/if}
 </div>
 
 <style>
